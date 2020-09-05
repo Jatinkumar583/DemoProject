729cb6959c57bfc9fed2a1f0c154b4b2335b56ba
mohitk6 <56404389+mohitk6@users.noreply.github.com>
2020-09-05 15:51:25 +0530

    new commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine("Even Number");
+                }
+            }
         }
     }
 }
